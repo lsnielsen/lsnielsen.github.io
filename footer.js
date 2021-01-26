c5065878d2c221127a860cb453fe206b115ed62d
Lars <lars.sondertoft@gmail.com>
2021-01-26 13:05:16 +0100

    Include footer

---
+++ footer.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+document.write(`
+
+
+        <!-- Footer -->
+        <footer class="py-5 bg-dark">
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+            </div>
+            <!-- /.container -->
+        </footer>
+
+`);
